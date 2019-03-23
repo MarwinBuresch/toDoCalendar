d74bf556045f1c418fe50820bc2aa3194617900a
MarwinBuresch <marwin@posteo.de>
2019-03-23 01:19:45 +0100

    lorem test

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function myFunction() {
     var node = document.createElement("LI");
-    var textnode = document.createTextNode("Water");
+    var textnode = document.createTextNode("Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium asperiores ab accusamus vero culpa quae impedit amet aut libero quasi deserunt vel repellat placeat deleniti, tempore sapiente cumque soluta labore dicta sint dignissimos quibusdam facilis mollitia? Eum voluptates ex officiis fugit aut, laudantium dolor impedit necessitatibus cumque voluptatum iusto officia");
     node.appendChild(textnode);
-    document.getElementById("myList").appendChild(node);
+    document.getElementById("project").appendChild(node);
   }
